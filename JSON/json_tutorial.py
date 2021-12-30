eadd6cac67a13391132a48708235efcd2669b5bd
stefanoracke <80729784+stefanoracke@users.noreply.github.com>
2021-12-30 12:12:11 -0300

    Uploading JSONtutorial

--- JSON/json_tutorial.py
+++ JSON/json_tutorial.py
@@ -43,9 +43,34 @@ def endcode_user(o):
     else:
         raise TypeError('Object of type User is not JSON serializable')
 
+from json import JSONEncoder
+class UserEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, User):
+            return {'name': o.name, 'age': o.age, o.__class__.__name__: True}
+        return JSONEncoder.default(self, o)
+
+
+
+user1JSON= UserEncoder().encode(user)
+print("using UserEncoder: ", user1JSON)
+
+
 userJSON = json.dumps(user, default=endcode_user)
 
 print("The User is: \n",userJSON)
 
 with open('user.json','w') as file:
     json.dump(userJSON,file)
+
+#is not an object then we use decode
+
+def decode_user(dct):
+    if User.__name__ in dct:
+        return User(name=dct['name'],age=dct['age'])
+
+    return dct
+
+user = json.loads(userJSON, object_hook=decode_user)
+
+print(f"Decode_user calling like an object:\n The name is:{user.name} \n The age is:{user.age}")
